64623bb10cdf5a8512584ff2a2a11fc7b704625b
StephenCatsamas <seca8193@gmail.com>
2019-12-25 22:00:36 +1100

    basic functionality completed

--- computer.py
+++ computer.py
@@ -1,3 +1,5 @@
+import time
+
 
 class RAM:
 
@@ -11,8 +13,13 @@ class RAM:
     def __str__(self):
         prnt = "-------RAM-------"
         for address in self.memory:
+            addresshigh = address//16
+            addresslow = address - addresshigh
+            
             prnt += "\n"
-            prnt += "{:08b}".format(address)
+            prnt += "{:04b}".format(addresshigh)
+            prnt += " "
+            prnt += "{:04b}".format(addresslow)
             prnt += "  "
             prnt += "{:02x}".format(address)
             prnt += "  "
@@ -35,21 +42,79 @@ class CPU:
         self.regA = 0b0
         self.regB = 0b0
         self.inst = 0b0
+        self.exef = 0b0
         self.ram = RAM
         
 
     def execute(self, val):
-        if val == 5:
-            print(self.inst)
+        if val == 0:
+            return
+
+        if val == 1:
+            self.inc_inst()
+            self.inst = self.read()
+            return
+
+        if val == 2:
+            self.inc_inst()
+            val = self.read()
+            self.regA = self.read(val)
+            return
+
+        if val == 3:
+            self.inc_inst()
+            val = self.read()
+            self.regB = self.read(val)
+            return
+
+        if val == 6:
+            self.inc_inst()
+            val = self.read()
+            self.write(val, self.regA)
+            return
+
+        if val == 7:
+            self.inc_inst()
+            val = self.read()
+            self.write(val, self.regB)
+            return
+
+        if val == 8:
+            self.regA = self.regA + self.regB
+            return
+
+        if val == 9:
+            self.regA = self.regA - self.regB
+            return
+
+        if val == 10:
+            self.inc_inst()
+            val = self.read()
+            if self.regA:
+                self.inst = val
+                return
+            
+        if val == 11:
+            self.inc_inst()
+            val = self.read()
+            if self.regB:
+                self.inst = val
+                return
 
     def inc_inst(self):
         self.inst += 1
         if self.inst == self.ram.size:
             self.inst = 0
 
-    def clock(self):
-        val = self.ram[self.inst]
+    def read(self, address = self.inst):
+        return self.ram[address]
 
+    def write(self, address, value):
+        self.ram[address] = value
+    
+    def clock(self):
+        val = self.read()
+        
         self.execute(val)
         
         self.inc_inst()
@@ -58,17 +123,18 @@ class CPU:
 MyRAM = RAM()
 
 
-MyRAM[4] = 5
+MyRAM[5] = 1
 
-MyRAM[10] = 5
+MyRAM[6] = 3
+
+print(MyRAM)
 
 
 MyCPU = CPU(MyRAM)
 
 while 1:
     MyCPU.clock()
-    input()
-    print("\r", end="")
+    time.sleep(1)
 
 
 
