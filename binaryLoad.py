dd70dc51821d2f94e8cb7ec95f4acba887a7e53c
Stephen Catsamas <seca8193@gmail.com>
2019-12-27 17:48:52 +1100

    added labeles to assembler

--- binaryLoad.py
+++ binaryLoad.py
@@ -14,19 +14,61 @@ class programmer:
             "IFA": 0x0a,
             "IFB": 0x0b
             }
+    lables = dict()
 
     def __init__(self, file):
 
         self.fp = file
         self.values = [0]*256
         self.counter = 0
+        self.cont = list()
         with open(self.fp, newline="") as csvfile:
-            self.reader = csv.reader(csvfile, delimiter=";", quotechar="|")
-            for row in self.reader:
+            reader = csv.reader(csvfile, delimiter=";", quotechar="|")
+            for row in reader:
+                self.cont.append(row)
+            for row in self.cont:
+                self.get_lables(row)
+            self.counter = 0
+            for row in self.cont:
                 self.interpret(row)
 
     def __iter__(self):
         return iter(self.values)
+
+    def get_lables(self,row):
+        print(row)
+        try:
+            row[0]
+        except IndexError:
+            return None
+
+        rstrp = row[0].strip()
+
+        if row[0].strip() == "":
+            return None
+            
+        if rstrp == "CMD":
+            if row[1].strip() == "ADR":
+                self.counter = eval(row[2].strip().replace(" ", ""))
+                return None
+            if row[1].strip() == "ADA":
+                self.counter += eval(row[2].strip().replace(" ", ""))
+                return None
+            if row[1].strip() == "ADM":
+                self.counter -= eval(row[2].strip().replace(" ", ""))
+                return None
+            return None
+            
+        if rstrp == "LAB":
+            if row[1].strip() == "DEF":
+                programmer.lables[row[2].strip()] = self.counter
+                return None
+
+        if rstrp in programmer.opcodes:
+            self.counter += 1
+            return None
+
+        self.counter += 1
 
     def interpret(self, row):
         print(row)
@@ -50,6 +92,14 @@ class programmer:
             if row[1].strip() == "ADM":
                 self.counter -= eval(row[2].strip().replace(" ", ""))
                 return None
+            return None
+        
+        if rstrp == "LAB":
+            if row[1].strip() == "REC":
+                self.values[self.counter] = programmer.lables[row[2].strip()]
+                self.counter += 1
+                return None
+            return None
 
         if rstrp in programmer.opcodes:
             self.values[self.counter] = programmer.opcodes[rstrp]
